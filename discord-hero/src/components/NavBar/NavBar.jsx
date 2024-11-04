73d93cd0ccaddedb2fff6230c77b9541513a99f7
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-11-04 16:26:45 +0100

    ex done

---
+++ discord-hero/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <nav>
+        <img src="../../assets/discord-logo-white.png" alt="Discord white logo" id='logo-img' />
+        <img src="../../assets/menu-icon.png" alt="menu icon" id='menu-icon'/>
+    </nav>
+  )
+}
+
+export default NavBar
